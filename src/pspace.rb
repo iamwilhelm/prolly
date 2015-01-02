39d180a4545d2a715a94dd9998eeac4a161d857d
Wil Chung <iamwil@gmail.com>
2015-01-02 03:15:21 -0800

    cache pspace calculations

--- src/pspace.rb
+++ src/pspace.rb
@@ -7,29 +7,62 @@ class PSpace
       @ps = PSpace.new(data)
     end
 
-    def rv(rand_vars)
-      RandVar.new(@ps, rand_vars)
+    def add(datum)
+      @ps ||= PSpace.new([])
+      @ps.add(datum)
+    end
+
+    def rv(*rand_vars)
+      if rand_vars.empty?
+        @ps.rand_vars
+      else
+        RandVar.new(@ps, *rand_vars)
+      end
+    end
+
+    def stash
+      @ps.stash
+    end
+
+    def uniq_vals(name)
+      @ps.uniq_vals(name)
     end
   end
 
   def initialize(data)
     @data = data
+    @uniq_vals = {}
+    @stash ||= {}
+  end
+
+  def stash
+    @stash
+  end
+
+  def add(datum)
+    @data << datum
   end
 
   def count(name, val)
     if val.nil?
-      @data.count { |e| e.has_key?(name) }
+      @stash["#{name}|#{val}"] ||= @data.count { |e| e.has_key?(name) }
     else
-      @data.count { |e| e[name] == val }
+      @stash["#{name}|#{val}"] ||= @data.count { |e| e[name] == val }
     end
   end
 
   def count2(name1, val1, name2, val2)
-    @data.count { |e| e[name1] == val1 and e[name2] == val2 }
+    @stash["#{name1}|#{val1} #{name2}|#{val2}"] ||= @data.count { |e|
+      e[name1] == val1 and e[name2] == val2
+    }
   end
 
   def uniq_vals(name)
-    @data.map { |li| li.has_key?(name) ? li[name] : nil }.uniq
+    @uniq_vals[name] ||= @data.map { |li| li.has_key?(name) ? li[name] : nil }.uniq
+  end
+
+  def rand_vars
+    @data.first.keys
   end
 
 end
